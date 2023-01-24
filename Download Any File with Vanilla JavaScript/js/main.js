0edbd0c4651e95091f35cb2ee27ad4af39afb6f5
sunitsen <sunitsen50@gmail.com>
2023-01-24 19:04:12 +0600

    message

---
+++ Download Any File with Vanilla JavaScript/js/main.js
@@ -0,0 +1,31 @@
+//selection
+const fileInput = document.querySelector("input"),
+downloadBtn = document.querySelector("button");
+elements = document.getElementById('elements')
+
+
+downloadBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    downloadBtn.innerHTML = 'Download file...';
+     fetchFile(fileInput.value);
+});
+
+function fetchFile(url){
+    fetch(url).then(res => res.blob()).then(file => {
+    
+     let tempUrel = URL.createObjectURL(file);
+     let aTag = document.createElement("a");
+     aTag.href = tempUrel;
+     aTag.download = url.replace(/.*[\\\/]/, '');
+     elements.appendChild(aTag);
+    aTag.click();
+    aTag.remove();
+    URL.revokeObjectURL(tempUrel);
+    downloadBtn.innerHTML = 'Download file';
+
+    }).catch(() => {
+        downloadBtn.innerHTML = 'Download file';
+        alert("Failed to download fiel");
+    });
+}
+
